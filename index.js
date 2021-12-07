4bd103c2721f04b38dcabef48671837876e9240a
Greg Schmitt <gregjschmitt@gmail.com>
2021-12-07 15:24:23 -0500

    commit tree 4f78413de141ca4341590d2a2bae5815c9225030

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+
+  const formData = {
+    name: name,
+    email: email,
+  };
+  
+  const configObj = {
+    method: "POST", 
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return (
+    fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(obj => {
+      const p = document.createElement("p");
+      p.textContent = obj.id;
+      document.querySelector("body").append(p);
+    })
+    .catch(err => {
+      const p = document.createElement("p");
+      p.textContent = err.message;
+      document.querySelector("body").append(p);
+    })
+  );
+
+}
+
